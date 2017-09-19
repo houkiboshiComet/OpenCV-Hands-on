fd97d70f19c91247749c6ad3569e8c35f7f85220
houkiboshiComet <wish.on.broomystars@gmail.com>
2017-09-19 17:16:41 +0900

    add comment for important function

--- CvController/ImageProcessor.cpp
+++ CvController/ImageProcessor.cpp
@@ -21,8 +21,14 @@ namespace OpenCVApp {
 
 	}
 
+	/**
+	* @brief <Handson EX1> 画像をリードする。
+	* @param path 画像ファイルのパス
+	* @param out Mat形式の画像の書き出し先
+	* @detail 画像はCV_8UC3形式のフルカラー画像として読み込まれる。
+	*/
 	void ImageProcessor::readImage(const std::string& path, cv::Mat* out) {
-		*out = cv::imread(path, cv::IMREAD_COLOR);
+		*out = cv::imread(path, cv::IMREAD_COLOR); 
 	}
 	bool ImageProcessor::writeImage(const std::string& path, const cv::Mat* in) {
 		return cv::imwrite(path, *in);
@@ -30,6 +36,15 @@ namespace OpenCVApp {
  	}
 
 	const int MaxWitdh::BRIGHTNESS = 255;
+	/**
+	* @brief <Handson EX2> 画像のRGBの毎の明るさを変更する。
+	* @param src 変換元の画像
+	* @param r 赤色の明るさ ( -MaxWitdh::BRIGHTNESS <= r <= MaxWitdh::BRIGHTNESS )
+	* @param g 緑色の明るさ ( -MaxWitdh::BRIGHTNESS <= g <= MaxWitdh::BRIGHTNESS )
+	* @param b 青色の明るさ ( -MaxWitdh::BRIGHTNESS <= b <= MaxWitdh::BRIGHTNESS )
+	* @param dst 変換先の画像
+	* @detail srcをrgb毎のMatに分解し、それぞれの明るさを変更する。
+	*/
 	void ImageProcessor::changeBrightness(const cv::Mat* src, int r, int g, int b, cv::Mat* dst) {
 		toSafeValue<int>(-MaxWitdh::BRIGHTNESS, &r, MaxWitdh::BRIGHTNESS);
 		toSafeValue<int>(-MaxWitdh::BRIGHTNESS, &g, MaxWitdh::BRIGHTNESS);
@@ -47,6 +62,13 @@ namespace OpenCVApp {
 	}
 
 	const int MaxLevel::BLUR = 10;
+	/**
+	* @brief <Handson EX3> 画像にぼかし効果を加える。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::BLUR )
+	* @param dst 変換先の画像
+	* @detail levelに比例してぼかしの効果は強くなる。
+	*/
 	void ImageProcessor::blur(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::BLUR);
 
@@ -55,6 +77,13 @@ namespace OpenCVApp {
 	}
 
 	const int MaxLevel::SHARPNESS = 10;
+	/**
+	* @brief <Handson EX4> 画像に鮮鋭化効果を加える。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::SHARPNESS )
+	* @param dst 変換先の画像
+	* @detail levelに比例して鮮鋭化効果は強くなる。
+	*/
 	void ImageProcessor::sharpen(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::SHARPNESS);
 
@@ -91,6 +120,15 @@ namespace OpenCVApp {
 	}
 
 	const int MaxLevel::PENCIL = 10;
+	/**
+	* @brief <Handson EX5.1> 画像を黒鉛筆で描いたような線画画像に変換する。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::PENCIL )
+	* @param dst 変換先の画像
+	* @detail 画像からエッジを検出することで、画像内の輪郭を検出する。
+	* 輪郭を黒、背景を白とすることで、線画画像を得る。
+	* levelに比例して多くのエッジを検出する。
+	*/
 	void ImageProcessor::drawWithPenclil(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::PENCIL);
 
@@ -105,6 +143,13 @@ namespace OpenCVApp {
 		*dst = ~*dst;
 	}
 
+	/**
+	* @brief <Handson EX5.2> 画像を色鉛筆で描いたようなフルカラーの線画画像に変換する。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::PENCIL )
+	* @param dst 変換先の画像
+	* @detail 白黒の線画を作成し、マスクとすることでフルカラーの線画画像を作成する。
+	*/
 	void ImageProcessor::drawWithColorPenclil(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::PENCIL);
 
@@ -115,6 +160,13 @@ namespace OpenCVApp {
 	}
 
 	const int MaxLevel::OIL_PAINT = 10;
+	/**
+	* @brief <Handson EX6> 画像を油絵で書いたようなクラスタリングされた画像に変換する。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::OIL_PAINT )
+	* @param dst 変換先の画像
+	* @detail levelに比例して、クラスタリングの幅が拡大する。(大雑把なクラスタリングになる。)
+	*/
 	void ImageProcessor::toOilPaint(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::OIL_PAINT);
 
@@ -127,8 +179,15 @@ namespace OpenCVApp {
 
 #define RAND(max) (rand() % (max))
 #define DOUBLE_RAND() (((double) rand() * 2.0 / (double) RAND_MAX) - 1.0)
-	
 	const int MaxLevel::SNOW_STORM = 200;
+	/**
+	* @brief <Handson EX7> 画像に吹雪のような効果を加える。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::SNOW_STORM )
+	* @param dst 変換先の画像
+	* @detail levelに比例して、多くの雪が描画される。
+	* 雪の位置、形状は、アフィン変換により規則性とランダム性を持って決定される。
+	*/
 	void ImageProcessor::causeSnowStorm(const cv::Mat* src, int level, cv::Mat* dst) {
 		toSafeValue<int>(0, &level, MaxLevel::SNOW_STORM);
 		src->copyTo(*dst);
@@ -164,15 +223,23 @@ namespace OpenCVApp {
 	}
 
 
-	const int MaxLevel::ORIGINAL = 10;
-	void ImageProcessor::applyOriginalEffect(const cv::Mat* src, int level, cv::Mat* dst) {
-		/* duumy */
-
-	}
+	
 
 	const int MaxLevel::DETECTION = 10;
+	/**
+	* @brief <Handson EX8> 画像の中から目的の物体を含む矩形を検出し、物体矩形を描画する。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::DETECTION )
+	* @param cascade 物体検出のためのカスケード分類器クラスのオブジェクト
+	* @param minNeighbors 物体候補となる矩形に必要な、最小の近傍矩形数( 1 <= minNeighbors)
+	* @param dst 変換先の画像
+	* @detail levelに比例して、精密に物体検出を行う。
+	* 検出する物体は引数のcascadeに依存する。
+	*/
 	void ImageProcessor::detectObject(const cv::Mat* src, int level, cv::Mat* dst, cv::CascadeClassifier* cascade, int minNeighbors) {
-		toSafeValue<int>(0, &minNeighbors, MaxLevel::DETECTION);
+		toSafeValue<int>(0, &level, MaxLevel::DETECTION);
+		toSafeValue<int>(1, &minNeighbors, 0x7ffffff );
+
 		double scaleFactor = 1.55 - 0.5 * (level / (double) MaxLevel::DETECTION);
 
 		src->copyTo(*dst);
@@ -187,5 +254,17 @@ namespace OpenCVApp {
 		for (auto rect : rects) {
 			cv::rectangle(*dst, rect, cv::Scalar(100,255,100), 2);
 		}
+	}
+
+	const int MaxLevel::ORIGINAL = 10;
+	/**
+	* @brief <Handson EXα> 画像にオリジナルのエフェクトを加える。
+	* @param src 変換元の画像
+	* @param level 効果のレベル ( 0 <= level <=  MaxLevel::ORIGINAL )
+	* @param dst 変換先の画像
+	* @detail levelに比例して、オリジナルのエフェクトが加わる。
+	*/
+	void ImageProcessor::applyOriginalEffect(const cv::Mat* src, int level, cv::Mat* dst) {
+		src->copyTo(*dst);
 	}
 }
